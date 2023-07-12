da39b1568187decc93a77be38736f9a72fbb0c61
Vojislav Trujkic <voizlav@gmail.com>
2023-07-12 12:34:33 +0200

    Add list_years

---
+++ part8/03_list_of_years/list_years.py
@@ -0,0 +1,14 @@
+from datetime import date
+
+
+def list_years(dates: list) -> list:
+    return sorted([d.year for d in dates])
+
+
+if __name__ == "__main__":
+    date1 = date(2019, 2, 3)
+    date2 = date(2006, 10, 10)
+    date3 = date(1993, 5, 9)
+
+    years = list_years([date1, date2, date3])
+    print(years)
