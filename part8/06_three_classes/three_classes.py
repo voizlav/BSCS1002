35caf52f93bd49b795d3772177b2f4627bf6fe5a
Vojislav Trujkic <voizlav@gmail.com>
2023-07-15 12:28:28 +0200

    Add three_classes

---
+++ part8/06_three_classes/three_classes.py
@@ -0,0 +1,23 @@
+class Checklist:
+    def __init__(self, header: str, entries: list):
+        self.header = header
+        self.entries = entries
+
+
+class Customer:
+    def __init__(self, id: str, balance: int, discount: float):
+        self.id = id
+        self.balance = balance
+        self.discount = discount
+
+
+class Cable:
+    def __init__(self, model: str, length: float, max_speed: int, bidirectional: bool):
+        self.model = model
+        self.length = length
+        self.max_speed = max_speed
+        self.bidirectional = bidirectional
+
+
+if __name__ == "__main__":
+    pass
