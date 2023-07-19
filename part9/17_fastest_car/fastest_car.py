60ab896c2c87ae61d72284906ebef6355b75aba5
Vojislav Trujkic <voizlav@gmail.com>
2023-07-19 11:53:46 +0200

    Add fastest_car

---
+++ part9/17_fastest_car/fastest_car.py
@@ -0,0 +1,21 @@
+class Car:
+    def __init__(self, make: str, top_speed: int):
+        self.make = make
+        self.top_speed = top_speed
+
+    def __str__(self):
+        return f"Car (make: {self.make}, top speed: {self.top_speed})"
+
+
+def fastest_car(cars: list):
+    return max([(car.top_speed, car.make) for car in cars])[1]
+
+
+if __name__ == "__main__":
+    car1 = Car("Saab", 195)
+    car2 = Car("Lada", 110)
+    car3 = Car("Ferrari", 280)
+    car4 = Car("Trabant", 85)
+
+    cars = [car1, car2, car3, car4]
+    print(fastest_car(cars))
