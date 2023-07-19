6f364b328fcf339ffaf7c74ce6ee89330f34f0e4
Vojislav Trujkic <voizlav@gmail.com>
2023-07-19 12:05:28 +0200

    Add passing_submissions

---
+++ part9/18_passing_submissions/passing_submissions.py
@@ -0,0 +1,23 @@
+class ExamSubmission:
+    def __init__(self, examinee: str, points: int):
+        self.examinee = examinee
+        self.points = points
+
+    def __str__(self):
+        return f"ExamSubmission (examinee: {self.examinee}, points: {self.points})"
+
+
+def passed(submissions: list, lowest_passing: int):
+    return [s for s in submissions if s.points >= lowest_passing]
+
+
+if __name__ == "__main__":
+    s1 = ExamSubmission("Peter", 12)
+    s2 = ExamSubmission("Pippa", 19)
+    s3 = ExamSubmission("Paul", 15)
+    s4 = ExamSubmission("Phoebe", 9)
+    s5 = ExamSubmission("Persephone", 17)
+
+    passes = passed([s1, s2, s3, s4, s5], 15)
+    for passing in passes:
+        print(passing)
