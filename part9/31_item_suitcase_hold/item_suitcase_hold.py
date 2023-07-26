8a75f4531e59df4edefa5aea648432649282af0d
Vojislav Trujkic <voizlav@gmail.com>
2023-07-26 20:43:09 +0200

    Update item_suitcase_hold cargo hold

--- part9/31_item_suitcase_hold/item_suitcase_hold.py
+++ part9/31_item_suitcase_hold/item_suitcase_hold.py
@@ -39,18 +39,41 @@ class Suitcase:
 
 
 class CargoHold:
-    pass
+    def __init__(self, maximum_weight: int):
+        self.__maximum_weight = maximum_weight
+        self.__suitcases = []
+
+    def weight(self):
+        return sum(suitcase.weight() for suitcase in self.__suitcases)
+
+    def space(self):
+        return self.__maximum_weight - self.weight()
+
+    def add_suitcase(self, suitcase: Suitcase):
+        if self.__maximum_weight > self.weight() + suitcase.weight():
+            self.__suitcases.append(suitcase)
+
+    def __str__(self):
+        return f"{len(self.__suitcases)} {'suitcase' if len(self.__suitcases) == 1 else 'suitcases'}, space for {self.space()} kg"
 
 
 if __name__ == "__main__":
+    cargo_hold = CargoHold(1000)
+    print(cargo_hold)
+
     book = Item("ABC Book", 2)
     phone = Item("Nokia 3210", 1)
     brick = Item("Brick", 4)
 
-    suitcase = Suitcase(10)
-    suitcase.add_item(book)
-    suitcase.add_item(phone)
-    suitcase.add_item(brick)
+    adas_suitcase = Suitcase(10)
+    adas_suitcase.add_item(book)
+    adas_suitcase.add_item(phone)
 
-    heaviest = suitcase.heaviest_item()
-    print(f"The heaviest item: {heaviest}")
+    peters_suitcase = Suitcase(10)
+    peters_suitcase.add_item(brick)
+
+    cargo_hold.add_suitcase(adas_suitcase)
+    print(cargo_hold)
+
+    cargo_hold.add_suitcase(peters_suitcase)
+    print(cargo_hold)
