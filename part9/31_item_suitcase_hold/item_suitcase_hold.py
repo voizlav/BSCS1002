5d01cb55e0d53e75d426739a7dc07817a2c21fe3
Vojislav Trujkic <voizlav@gmail.com>
2023-07-26 19:41:05 +0200

    Update item_suitcase_hold item

--- part9/31_item_suitcase_hold/item_suitcase_hold.py
+++ part9/31_item_suitcase_hold/item_suitcase_hold.py
@@ -1,5 +1,16 @@
 class Item:
-    pass
+    def __init__(self, name: str, weight: int):
+        self.__name = name
+        self.__weight = weight
+
+    def name(self):
+        return self.__name
+
+    def weight(self):
+        return self.__weight
+
+    def __str__(self):
+        return f"{self.__name} ({self.__weight} kg)"
 
 
 class Suitcase:
