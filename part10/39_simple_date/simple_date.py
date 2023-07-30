76cca98ab8ca57bb5d4a4e6881e579fbbdd41649
Vojislav Trujkic <voizlav@gmail.com>
2023-07-30 18:28:09 +0200

    Update simple_date comparisons

--- part10/39_simple_date/simple_date.py
+++ part10/39_simple_date/simple_date.py
@@ -1,5 +1,26 @@
 class SimpleDate:
-    pass
+    def __init__(self, day: int, month: int, year: int):
+        self.__day = day
+        self.__month = month
+        self.__year = year
+
+    def __total_days(self):
+        return self.__day + self.__month * 30 + self.__year * 365
+
+    def __str__(self):
+        return f"{self.__day}.{self.__month}.{self.__year}"
+
+    def __eq__(self, other: 'SimpleDate'):
+        return self.__total_days() == other.__total_days()
+
+    def __ne__(self, other: 'SimpleDate'):
+        return self.__total_days() != other.__total_days()
+
+    def __gt__(self, other: 'SimpleDate'):
+        return self.__total_days() > other.__total_days()
+
+    def __lt__(self, other: 'SimpleDate'):
+        return self.__total_days() < other.__total_days()
 
 
 if __name__ == "__main__":
