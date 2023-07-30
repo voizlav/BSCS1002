efd379ff79d809a8b107adba58d16bc8318c310c
Vojislav Trujkic <voizlav@gmail.com>
2023-07-30 17:55:11 +0200

    Add simple_date initial

---
+++ part10/39_simple_date/simple_date.py
@@ -0,0 +1,16 @@
+class SimpleDate:
+    pass
+
+
+if __name__ == "__main__":
+    d1 = SimpleDate(4, 10, 2020)
+    d2 = SimpleDate(28, 12, 1985)
+    d3 = SimpleDate(28, 12, 1985)
+
+    print(d1)
+    print(d2)
+    print(d1 == d2)
+    print(d1 != d2)
+    print(d1 == d3)
+    print(d1 < d2)
+    print(d1 > d2)
