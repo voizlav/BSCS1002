55d6eddbe20089ffdf9e4a24dfe4cbc4a26cb1ed
Vojislav Trujkic <voizlav@gmail.com>
2023-07-30 11:30:35 +0200

    Update money equal amounts

--- part10/38_money/money.py
+++ part10/38_money/money.py
@@ -3,13 +3,23 @@ class Money:
         self.euros = euros
         self.cents = cents
 
+    def __money(self):
+        return self.euros + self.cents / 100
+
     def __str__(self):
-        return f"{self.euros + self.cents / 100:.2f} eur"
+        return f"{self.__money():.2f} eur"
+
+    def __eq__(self, other: 'Money'):
+        return self.__money() == other.__money()
 
 
 if __name__ == "__main__":
     e1 = Money(4, 10)
-    e2 = Money(2, 5)  # two euros and five cents
+    e2 = Money(2, 5)
+    e3 = Money(4, 10)
 
     print(e1)
     print(e2)
+    print(e3)
+    print(e1 == e2)
+    print(e1 == e3)
