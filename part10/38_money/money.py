e755accd116b442765aeeea1bc3ecd6508341dc8
Vojislav Trujkic <voizlav@gmail.com>
2023-07-30 12:01:23 +0200

    Update money the value must not be directly accessible

--- part10/38_money/money.py
+++ part10/38_money/money.py
@@ -1,10 +1,10 @@
 class Money:
     def __init__(self, euros: int, cents: int):
-        self.euros = euros
-        self.cents = cents
+        self.__euros = euros
+        self.__cents = cents
 
     def __money(self):
-        return self.euros + self.cents / 100
+        return self.__euros + self.__cents / 100
 
     def __str__(self):
         return f"{self.__money():.2f} eur"
@@ -22,12 +22,12 @@ class Money:
         return self.__money() > other.__money()
 
     def __add__(self, other: 'Money'):
-        return Money(self.euros + other.euros, self.cents + other.cents)
+        return Money(self.__euros + other.__euros, self.__cents + other.__cents)
 
     def __sub__(self, other: 'Money'):
         if self < other:
             raise ValueError("a negative result is not allowed")
-        return Money(self.euros - other.euros, self.cents - other.cents)
+        return Money(self.__euros - other.__euros, self.__cents - other.__cents)
 
 
 if __name__ == "__main__":
