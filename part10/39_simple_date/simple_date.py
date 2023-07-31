e2a881e1cbd6985561bce1bafcd1ee2621222446
Vojislav Trujkic <voizlav@gmail.com>
2023-07-31 11:00:35 +0200

    Update simple_date difference

--- part10/39_simple_date/simple_date.py
+++ part10/39_simple_date/simple_date.py
@@ -7,8 +7,8 @@ class SimpleDate:
         self.__year = year if year > 0 else 1
         self.__update()
 
-    def __update(self, days: int = 0):
-        total_days = self.__total_days() + days
+    def __update(self):
+        total_days = self.__total_days()
         self.__year = 1 + (total_days // 360)
         total_days %= 360
         self.__month = 1 + (total_days // 30)
@@ -33,17 +33,19 @@ class SimpleDate:
         return self.__total_days() < other.__total_days()
 
     def __add__(self, other: int):
-        return SimpleDate(self.__total_days() + other, 0, 0)
+        return SimpleDate(self.__day + other, self.__month, self.__year)
+
+    def __sub__(self, other: 'SimpleDate'):
+        return abs(self.__total_days() - other.__total_days())
 
 
 if __name__ == "__main__":
     d1 = SimpleDate(4, 10, 2020)
-    d2 = SimpleDate(28, 12, 1985)
+    d2 = SimpleDate(2, 11, 2020)
+    d3 = SimpleDate(28, 12, 1985)
 
-    d3 = d1 + 3
-    d4 = d2 + 400
-
-    print(d1)
-    print(d2)
-    print(d3)
-    print(d4)
+    print(d2 - d1)
+    print(d1 - d2)
+    print(d1 - d3)
+    print(d1 + 3)
+    print(d2 + 400)
