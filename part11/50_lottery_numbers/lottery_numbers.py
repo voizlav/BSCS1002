8b087ec429d655acdd4454a62bebe3954a165e8d
Vojislav Trujkic <voizlav@gmail.com>
2023-08-16 04:07:55 +0200

    Add lottery_numbers

--- part11/50_lottery_numbers/lottery_numbers.py
+++ part11/50_lottery_numbers/lottery_numbers.py
@@ -3,12 +3,18 @@ class LotteryNumbers:
         self.week = week
         self.numbers = numbers
 
-    def number_of_hits(self, numbers: list):
+    def number_of_hits(self, numbers: []):
         return len([num for num in numbers if num in self.numbers])
+
+    def hits_in_place(self, numbers: []):
+        return [num if num in self.numbers else -1 for num in numbers]
 
 
 if __name__ == "__main__":
     week5 = LotteryNumbers(5, [1, 2, 3, 4, 5, 6, 7])
     my_numbers = [1, 4, 7, 11, 13, 19, 24]
-
     print(week5.number_of_hits(my_numbers))
+
+    week8 = LotteryNumbers(8, [1, 2, 3, 10, 20, 30, 33])
+    my_numbers = [1, 4, 7, 10, 11, 20, 30]
+    print(week8.hits_in_place(my_numbers))
