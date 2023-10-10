ac35f120f47c38123e23c69a372c9e87779c1b43
Vojislav Trujkic <voizlav@gmail.com>
2023-10-11 00:07:14 +0200

    Update order book - order book

--- part11/61_order_book/order_book.py
+++ part11/61_order_book/order_book.py
@@ -25,15 +25,31 @@ class Task:
         return f"{self.id}: {self.description} ({self.workload} hours), programmer {self.programmer} {self.get_status()}"
 
 
+class OrderBook:
+    def __init__(self):
+        self.tasks = []
+
+    def add_order(self, description: str, programmer: str, workload: int):
+        task = Task(description=description, programmer=programmer, workload=workload)
+        self.tasks.append(task)
+
+    def all_orders(self):
+        return self.tasks
+
+    def programmers(self):
+        return set(task.programmer for task in self.all_orders())
+
+
 if __name__ == "__main__":
-    t1 = Task("program hello world", "Eric", 3)
-    print(t1.id, t1.description, t1.programmer, t1.workload)
-    print(t1)
-    print(t1.is_finished())
-    t1.mark_finished()
-    print(t1)
-    print(t1.is_finished())
-    t2 = Task("program webstore", "Adele", 10)
-    t3 = Task("program mobile app for workload accounting", "Eric", 25)
-    print(t2)
-    print(t3)
+    orders = OrderBook()
+    orders.add_order("program webstore", "Adele", 10)
+    orders.add_order("program mobile app for workload accounting", "Eric", 25)
+    orders.add_order("program app for practising mathematics", "Adele", 100)
+
+    for order in orders.all_orders():
+        print(order)
+
+    print()
+
+    for programmer in orders.programmers():
+        print(programmer)
