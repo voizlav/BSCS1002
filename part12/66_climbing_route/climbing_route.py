729de9b6e5cbe9233e72f9de69d89d84743d60b4
Vojislav Trujkic <voizlav@gmail.com>
2023-10-11 21:36:56 +0200

    Update climbing route - sort by length

--- part12/66_climbing_route/climbing_route.py
+++ part12/66_climbing_route/climbing_route.py
@@ -8,4 +8,20 @@ class ClimbingRoute:
         return f"{self.name}, length {self.length} metres, grade {self.grade}"
 
 
-# TODO
+def sort_by_length(routes: list):
+    def order_length(route: ClimbingRoute):
+        return route.length
+
+    return sorted(routes, key=order_length, reverse=True)
+
+
+if __name__ == "__main__":
+    r1 = ClimbingRoute("Edge", 38, "6A+")
+    r2 = ClimbingRoute("Smooth operator", 11, "7A")
+    r3 = ClimbingRoute("Synchro", 14, "8C+")
+    r4 = ClimbingRoute("Small steps", 12, "6A+")
+
+    routes = [r1, r2, r3, r4]
+
+    for route in sort_by_length(routes):
+        print(route)
