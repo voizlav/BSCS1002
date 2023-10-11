85c66312f3c25698910f4c0e82283b2a4d00ed6a
Vojislav Trujkic <voizlav@gmail.com>
2023-10-12 00:37:59 +0200

    Update ballplayers - most goals

--- part12/68_ballplayers/ballplayers.py
+++ part12/68_ballplayers/ballplayers.py
@@ -11,3 +11,18 @@ class BallPlayer:
             f"BallPlayer(name={self.name}, number={self.number}, "
             f"goals={self.goals}, passes={self.passes}, minutes={self.minutes})"
         )
+
+
+def most_goals(team: list):
+    return max(team, key=lambda player: player.goals)
+
+
+if __name__ == "__main__":
+    player1 = BallPlayer("Archie Bonkers", 13, 5, 12, 46)
+    player2 = BallPlayer("Speedy Tickets", 7, 2, 26, 55)
+    player3 = BallPlayer("Cruella De Hill", 9, 1, 32, 26)
+    player4 = BallPlayer("Devilled Tasmanian", 12, 1, 11, 41)
+    player5 = BallPlayer("Donald Quack", 4, 3, 9, 12)
+
+    team = [player1, player2, player3, player4, player5]
+    print(most_goals(team))
