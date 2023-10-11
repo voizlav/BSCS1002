54fd2f2aae670c17652246614bd0ea95ee3484bb
Vojislav Trujkic <voizlav@gmail.com>
2023-10-12 00:15:27 +0200

    Update climbing area - sort by number of routes

--- part12/67_climbing_area/climbing_area.py
+++ part12/67_climbing_area/climbing_area.py
@@ -24,9 +24,35 @@ class ClimbingArea:
             return route.grade
 
         routes_in_order = sorted(self.__routes, key=by_difficulty)
-        # last route
         return routes_in_order[-1]
 
     def __str__(self):
         hardest_route = self.hardest_route()
         return f"{self.name} {self.routes()} routes, hardest {hardest_route.grade}"
+
+
+def sort_by_number_of_routes(areas: list):
+    def order_by_routes(area: ClimbingArea):
+        return area.routes()
+
+    return sorted(areas, key=order_by_routes)
+
+
+if __name__ == "__main__":
+    ca1 = ClimbingArea("Olhava")
+    ca1.add_route(ClimbingRoute("Edge", 38, "6A+"))
+    ca1.add_route(ClimbingRoute("Great cut", 36, "6B"))
+    ca1.add_route(ClimbingRoute("Swedish route", 42, "5+"))
+
+    ca2 = ClimbingArea("Nummi")
+    ca2.add_route(ClimbingRoute("Synchro", 14, "8C+"))
+
+    ca3 = ClimbingArea("Nalkkila slab")
+    ca3.add_route(ClimbingRoute("Small steps", 12, "6A+"))
+    ca3.add_route(ClimbingRoute("Smooth operator", 11, "7A"))
+    ca3.add_route(ClimbingRoute("Piggy not likey", 12, "6B+"))
+    ca3.add_route(ClimbingRoute("Orchard", 8, "6A"))
+
+    areas = [ca1, ca2, ca3]
+    for area in sort_by_number_of_routes(areas):
+        print(area)
