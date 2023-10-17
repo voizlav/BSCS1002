34d3cb1c092fe4f74e67471c11471b3f697baa2c
Vojislav Trujkic <voizlav@gmail.com>
2023-10-18 00:24:13 +0200

    Update filtering attempts - accepted attempts

--- part12/74_filtering_attempts/filtering_attempts.py
+++ part12/74_filtering_attempts/filtering_attempts.py
@@ -8,3 +8,16 @@ class CourseAttempt:
         return (
             f"{self.student_name}, grade for the course {self.course_name} {self.grade}"
         )
+
+
+def accepted(attempts: list):
+    return filter(lambda attempt: attempt.grade >= 1, attempts)
+
+
+if __name__ == "__main__":
+    s1 = CourseAttempt("Peter Python", "Introduction to Programming", 3)
+    s2 = CourseAttempt("Olivia C. Objective", "Introduction to Programming", 5)
+    s3 = CourseAttempt("Peter Python", "Advanced Course in Programming", 0)
+
+    for attempt in accepted([s1, s2, s3]):
+        print(attempt)
