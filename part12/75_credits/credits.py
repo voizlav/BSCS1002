230b152ae34c49ce112f10bfe276da5d2da00e53
Vojislav Trujkic <voizlav@gmail.com>
2023-10-18 05:29:10 +0200

    Update credits - average grade for passed course

--- part12/75_credits/credits.py
+++ part12/75_credits/credits.py
@@ -20,6 +20,11 @@ def sum_of_passed_credits(credits: list):
     return reduce(lambda total, attempt: total + attempt.credits, credits, 0)
 
 
+def average(grades: list):
+    grades = list(filter(lambda attempt: attempt.grade > 0, grades))
+    return reduce(lambda total, attempt: total + attempt.grade, grades, 0) / len(grades)
+
+
 if __name__ == "__main__":
     s1 = CourseAttempt("Introduction to Programming", 5, 5)
     s2 = CourseAttempt("Advanced Course in Programming", 0, 4)
@@ -30,3 +35,6 @@ if __name__ == "__main__":
 
     credit_sum = sum_of_passed_credits([s1, s2, s3])
     print(credit_sum)
+
+    ag = average([s1, s2, s3])
+    print(ag)
