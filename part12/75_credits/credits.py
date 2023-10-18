26a7547ef1ce9c65821c8b7c65b68894f8a731b5
Vojislav Trujkic <voizlav@gmail.com>
2023-10-18 05:10:16 +0200

    Update credits - the sum of all credits

--- part12/75_credits/credits.py
+++ part12/75_credits/credits.py
@@ -11,9 +11,13 @@ class CourseAttempt:
         return f"{self.course_name} ({self.credits} cr) grade {self.grade}"
 
 
+def sum_of_all_credits(credits: list):
+    return reduce(lambda total, attempt: total + attempt.credits, credits, 0)
+
+
 if __name__ == "__main__":
-    attempt = CourseAttempt("Data Structures and Algorithms", 3, 10)
-    print(attempt)
-    print(attempt.course_name)
-    print(attempt.credits)
-    print(attempt.grade)
+    s1 = CourseAttempt("Introduction to Programming", 5, 5)
+    s2 = CourseAttempt("Advanced Course in Programming", 4, 5)
+    s3 = CourseAttempt("Data Structures and Algorithms", 3, 10)
+    credit_sum = sum_of_all_credits([s1, s2, s3])
+    print(credit_sum)
