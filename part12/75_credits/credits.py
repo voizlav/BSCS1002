c16ec9a4dffbdd4a8f4f9b3cbab32503643452a9
Vojislav Trujkic <voizlav@gmail.com>
2023-10-18 05:20:33 +0200

    Update credits - the sum of passed credits

--- part12/75_credits/credits.py
+++ part12/75_credits/credits.py
@@ -15,9 +15,18 @@ def sum_of_all_credits(credits: list):
     return reduce(lambda total, attempt: total + attempt.credits, credits, 0)
 
 
+def sum_of_passed_credits(credits: list):
+    credits = filter(lambda attempt: attempt.grade > 0, credits)
+    return reduce(lambda total, attempt: total + attempt.credits, credits, 0)
+
+
 if __name__ == "__main__":
     s1 = CourseAttempt("Introduction to Programming", 5, 5)
-    s2 = CourseAttempt("Advanced Course in Programming", 4, 5)
+    s2 = CourseAttempt("Advanced Course in Programming", 0, 4)
     s3 = CourseAttempt("Data Structures and Algorithms", 3, 10)
+
     credit_sum = sum_of_all_credits([s1, s2, s3])
+    print(credit_sum)
+
+    credit_sum = sum_of_passed_credits([s1, s2, s3])
     print(credit_sum)
