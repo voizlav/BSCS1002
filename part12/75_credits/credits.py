52e18fdd25d6dcd553be3467ac1796a2659530c5
Vojislav Trujkic <voizlav@gmail.com>
2023-10-18 05:07:08 +0200

    Update credits initial

--- part12/75_credits/credits.py
+++ part12/75_credits/credits.py
@@ -9,3 +9,11 @@ class CourseAttempt:
 
     def __str__(self):
         return f"{self.course_name} ({self.credits} cr) grade {self.grade}"
+
+
+if __name__ == "__main__":
+    attempt = CourseAttempt("Data Structures and Algorithms", 3, 10)
+    print(attempt)
+    print(attempt.course_name)
+    print(attempt.credits)
+    print(attempt.grade)
