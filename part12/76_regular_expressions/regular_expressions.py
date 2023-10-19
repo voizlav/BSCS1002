99dbbc9ffa4f27cc636e3b12d751ef1186c11401
Vojislav Trujkic <voizlav@gmail.com>
2023-10-19 22:18:24 +0200

    Update regular expressions - time of day

--- part12/76_regular_expressions/regular_expressions.py
+++ part12/76_regular_expressions/regular_expressions.py
@@ -15,6 +15,13 @@ def all_vowels(my_string: str):
     return False
 
 
+def time_of_day(my_string: str):
+    pattern = "^([0-1][0-9]|2[0-3])(:[0-5][0-9]){2}$"
+    if re.search(pattern, my_string):
+        return True
+    return False
+
+
 if __name__ == "__main__":
     print(is_dotw("Mon"))
     print(is_dotw("Fri"))
@@ -23,3 +30,8 @@ if __name__ == "__main__":
 
     print(all_vowels("eioueioieoieou"))
     print(all_vowels("autoooo"))
+
+    print(time_of_day("12:43:01"))
+    print(time_of_day("AB:01:CD"))
+    print(time_of_day("17:59:59"))
+    print(time_of_day("33:66:77"))
