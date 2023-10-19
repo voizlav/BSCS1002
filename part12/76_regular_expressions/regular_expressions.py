2115cb960467c60cd8839b83ec8cf7c4f12d14b1
Vojislav Trujkic <voizlav@gmail.com>
2023-10-19 22:00:03 +0200

    Update regular expressions - check the vowels

--- part12/76_regular_expressions/regular_expressions.py
+++ part12/76_regular_expressions/regular_expressions.py
@@ -8,8 +8,18 @@ def is_dotw(my_string: str):
     return False
 
 
+def all_vowels(my_string: str):
+    pattern = "^[aeiou]+$"
+    if re.search(pattern, my_string, re.IGNORECASE):
+        return True
+    return False
+
+
 if __name__ == "__main__":
     print(is_dotw("Mon"))
     print(is_dotw("Fri"))
     print(is_dotw("Tui"))
     print(is_dotw("Sunny"))
+
+    print(all_vowels("eioueioieoieou"))
+    print(all_vowels("autoooo"))
