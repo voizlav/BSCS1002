bd908dc7bb5dfb4e3fd58cf970af8c1fadfaf0ea
Vojislav Trujkic <voizlav@gmail.com>
2023-10-19 23:11:45 +0200

    Update hockey statistics - Add player class

--- part12/77_hockey_statistics/hockey_statistics.py
+++ part12/77_hockey_statistics/hockey_statistics.py
@@ -1,1 +1,18 @@
-# TODO
+class Player:
+    def __init__(
+        self,
+        name: str,
+        nationality: str,
+        assists: int,
+        goals: int,
+        penalties: int,
+        team: str,
+        games: int,
+    ):
+        self.name = name
+        self.nationality = nationality
+        self.assists = assists
+        self.goals = goals
+        self.penalties = penalties
+        self.team = team
+        self.games = games
