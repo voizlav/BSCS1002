60b7c60caaf5f54d1d5fcaf9a81686363b7c32a6
Vojislav Trujkic <voizlav@gmail.com>
2023-10-19 23:16:10 +0200

    Update hockey statistics - Add basic players class

--- part12/77_hockey_statistics/hockey_statistics.py
+++ part12/77_hockey_statistics/hockey_statistics.py
@@ -16,3 +16,11 @@ class Player:
         self.penalties = penalties
         self.team = team
         self.games = games
+
+
+class Players:
+    def __init__(self):
+        players = []
+
+    def add_player(self, player: Player):
+        self.players.append(player)
