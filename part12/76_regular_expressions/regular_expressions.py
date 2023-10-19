1c262daf17f066eedc103b513ee19793a25e094d
Vojislav Trujkic <voizlav@gmail.com>
2023-10-19 21:55:11 +0200

    Update regular expressions - days of the week

--- part12/76_regular_expressions/regular_expressions.py
+++ part12/76_regular_expressions/regular_expressions.py
@@ -1,2 +1,15 @@
-# Write your solution here
 import re
+
+
+def is_dotw(my_string: str):
+    pattern = "^(mon|tue|wed|thu|fri|sat|sun)$"
+    if re.search(pattern, my_string, re.IGNORECASE):
+        return True
+    return False
+
+
+if __name__ == "__main__":
+    print(is_dotw("Mon"))
+    print(is_dotw("Fri"))
+    print(is_dotw("Tui"))
+    print(is_dotw("Sunny"))
