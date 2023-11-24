65663d18ef5b4f457ff72ae6ac9f6d9874b82217
Vojislav Trujkic <voizlav@gmail.com>
2023-11-24 23:12:56 +0100

    Add four walls

---
+++ part13/89_four_walls/four_walls.py
@@ -0,0 +1,51 @@
+import pygame
+
+WIDTH = 640
+HEIGHT = 480
+
+pygame.init()
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+robot = pygame.image.load("robot.png")
+w = robot.get_width()
+h = robot.get_height()
+
+direction = 0
+x = 0
+y = 0
+
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                direction = 1
+            if event.key == pygame.K_RIGHT:
+                direction = 2
+            if event.key == pygame.K_UP:
+                direction = 3
+            if event.key == pygame.K_DOWN:
+                direction = 4
+
+        if event.type == pygame.KEYUP:
+            if event.key in [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]:
+                direction = 0
+
+    if direction == 1 and x >= 0:
+        x -= 2
+    if direction == 2 and x + w <= WIDTH:
+        x += 2
+    if direction == 3 and y >= 0:
+        y -= 2
+    if direction == 4 and y + h <= HEIGHT:
+        y += 2
+
+    window.fill((0, 0, 0))
+    window.blit(robot, (x, y))
+    pygame.display.flip()
+
+    clock.tick(60)
